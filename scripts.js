c19a0b89c99e2b13d76045fe4be7764c4ba1f506
yourUsername <your_email@example.com>
2016-12-01 14:35:45 -0500

    Added reset button, more test questions, refactored questionBank array

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 //Variables
-let $quest = $('#question');
+let $question = $('#question');
 let $ansA = $('#answerA');
 let $ansB = $('#answerB');
 let $ansC = $('#answerC');
@@ -7,64 +7,110 @@ let $ansD = $('#answerD');
 let $ansR = $('#answerR');
 
 //The Question and Answer Object
-let trivia = {
-  answer:"test"};
+let answer;
 
 //Functions
 //Populate trivia: Questions & Answers A, B, C, D
-let getQuestion = function (){
-      i = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
-      console.log(i);
-      console.log(setA[i]);
-      questions = setA[i];
-      //passes answer & question from questions[] to Answer Object
-      console.log(questions[0]);
-      console.log(questions[1]);
-      trivia.question = questions[0];
-      trivia.answer = questions[1];
-      //Randomize the placement of choices A,B,C,D
-      //randomizeChoice();
-      //Push choices up into DOM Elements
-      appendChoices();
+let getQuestion = function() {
+  let maxOptions = 6;
+  let minOptions = 0;
+  let randomQuestionBankIndex = Math.floor(Math.random() * (maxOptions - minOptions + 1)) + 0;
+  let i = randomQuestionBankIndex;
+    console.log(i);
+    console.log(questionBank[i]);
+    questionSet = questionBank[i];
+    //passes answer & question from questionSet[0] to answer variable
+    answer = questionSet[1][0];
+    console.log(questionSet[1][0]);
+    //Randomize the placement of choices A,B,C,D
+    randomizeChoice();
 };
 
-//Push choices up into DOM Elements
-let appendChoices = function (){
-      $ansA.append(questions[1]);
-      $ansB.append(questions[2]);
-      $ansC.append(questions[3]);
-      $ansD.append(questions[4]);
-      $quest.append(questions[0]);
-};
 
 //Randomize answer positions
-// let randomizeChoice = function (){
-//
-//
-//   questions.sort(function () {return 0.5 - Math.random()});
-//   //push array items (questions) to span tags
-// };
+let randomizeChoice = function() {
+    let x = questionSet[1].sort(function() {
+        return 0.5 - Math.random();
+    });
+    console.log(x);
+
+
+    //Push choices up into DOM Elements
+    let appendChoices = function() {
+        $ansA.append(x[0]);
+        $ansB.append(x[1]);
+        $ansC.append(x[2]);
+        $ansD.append(x[3]);
+        $question.append(questionSet[0]);
+    };
+    //Push choices up into DOM Elements
+    appendChoices();
+};
+
+
+//Compare selected choice to the answer in the Answer Object
+let correctAnswer = function() {
+    if (this.choice == answer) {
+        console.log('WIN');
+        resetAnswer();
+    } else {
+        console.log('LOSE');
+    }
+};
+
+
+//Resets Question set
+let resetAnswer = function() {
+    $question.html('');
+    $ansA.html('');
+    $ansB.html('');
+    $ansC.html('');
+    $ansD.html('');
+    console.log('Removed Question Set');
+};
 
 
 // Elements clicks
-$ansA.on('click', function(){
-  console.log('test');
+$ansA.on('click', function() {
+    console.log('testing answer');
+    let choice = $ansA.text();
+    correctAnswer();
 });
-$ansB.on('click', function(){
-  console.log('test');
+
+
+$ansB.on('click', function() {
+    console.log('test');
+    let choice = $ansB.text();
+    correctAnswer();
 });
-$ansC.on('click', function(){
-  console.log('test');
+
+
+$ansC.on('click', function() {
+    console.log('test');
+    let choice = $ansC.text();
+    correctAnswer();
 });
-$ansD.on('click', function(){
-  console.log('test');
+
+
+$ansD.on('click', function() {
+    console.log('test');
+    let choice = $ansD.text();
+    correctAnswer();
 });
 
 $('#start').on('click', getQuestion);
+$('#restart').on('click', resetAnswer);
+
+
+
 
 //Questions array
-setA = [
-  ["What was the name of Harry Potter's Defense Against The Dark Arts professor?", "Albus Dumbledore", "Samwise Gamgee", "Severus Snape", "Hans Gruber"],
-  ["What was the name of Harry Potter's dark arts professor?", "Albus Dumbledore", "Samwise Gamgee", "Severus Snape", "Hans Gruber"],
-  ["What was the name of Harry Potter's dark arts professor?", "Albus Dumbledore", "Samwise Gamgee", "Severus Snape", "Hans Gruber"],
+const questionBank = [
+    ['Who is the "Chosen One"?', ['Agent Smith', 'Neo', 'Harry Potter', 'The Rock']],
+    ["Who is Harry Potter's Defense Against The Dark Arts professor?", ['Severus Snape', 'Agent Smith', 'Neo', 'Harry Potter, himself']],
+    ['What is the name of the story Bilbo wrote about his adventures?', ['The Hobbit by Bilbo Baggins', 'The Silmarillion by Bilbo Baggins', 'A Hobbits Tale by Bilbo Baggins', 'Into the West by Bilbo Baggins']],
+    ['By what name do the Elves call Gandalf?', ['Mithrandir', 'Gandalf the Grey', 'Incanus', 'The Grey Pilgrim']],
+    ['What three swords were found in the Trolls Cave in The Hobbit?', ['Orcrist, Sting and Glamdring', 'Narsil, Glamdring and Hadhafang', 'Sting, Anduil and The White Knife of Legolas', 'Aeglos, Orcrist and Sting']],
+    ['Who is referred to as the Son of Glion?', ['Gimli', 'Durin', 'Balin', 'Legolas']],
+    ['What is "...as hard as dragon scale and as light as a feather"?', ['Mithril', 'Adamantium', 'Titanuim', 'The Rock']]
 ];
