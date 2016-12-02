bf676ddcd8ee7bb77603c42515cdb100b549063f
yourUsername <your_email@example.com>
2016-12-02 10:43:23 -0500

    Added javascript for questionBank, timer countdown, play message

--- scripts.js
+++ scripts.js
@@ -5,23 +5,24 @@ let $ansB = $('#answerB');
 let $ansC = $('#answerC');
 let $ansD = $('#answerD');
 let $ansR = $('#answerR');
-
-//The Question and Answer Object
+let $timer = $('#timer');
+let timeRemaining = 11;
 let answer;
+
 
 //Functions
 //Populate trivia: Questions & Answers A, B, C, D
 let getQuestion = function() {
-  let maxOptions = 6;
-  let minOptions = 0;
-  let randomQuestionBankIndex = Math.floor(Math.random() * (maxOptions - minOptions + 1)) + 0;
-  let i = randomQuestionBankIndex;
+    let maxOptions = 4;
+    let minOptions = 0;
+    let randomQuestionBankIndex = Math.floor(Math.random() * (maxOptions - minOptions + 1)) + 0;
+    let i = randomQuestionBankIndex;
     console.log(i);
     console.log(questionBank[i]);
     questionSet = questionBank[i];
     //passes answer & question from questionSet[0] to answer variable
-    answer = questionSet[1][0];
-    console.log(questionSet[1][0]);
+    answer = questionSet.answer;
+    console.log(questionSet.answer);
     //Randomize the placement of choices A,B,C,D
     randomizeChoice();
 };
@@ -29,35 +30,45 @@ let getQuestion = function() {
 
 //Randomize answer positions
 let randomizeChoice = function() {
-    let x = questionSet[1].sort(function() {
+    let shuffled = questionSet.answer_choices.sort(function() {
         return 0.5 - Math.random();
     });
-    console.log(x);
+    console.log(shuffled);
+    appendChoices(shuffled);
+};
 
 
-    //Push choices up into DOM Elements
-    let appendChoices = function() {
-        $ansA.append(x[0]);
-        $ansB.append(x[1]);
-        $ansC.append(x[2]);
-        $ansD.append(x[3]);
-        $question.append(questionSet[0]);
-    };
-    //Push choices up into DOM Elements
-    appendChoices();
+//Push choices up into DOM Elements
+let appendChoices = function(shuffled) {
+    $ansA.append(shuffled[0]);
+    $ansB.append(shuffled[1]);
+    $ansC.append(shuffled[2]);
+    $ansD.append(shuffled[3]);
+    $question.append(questionSet.question);
 };
 
 
 //Compare selected choice to the answer in the Answer Object
-let correctAnswer = function() {
-    if (this.choice == answer) {
+let correctAnswer = function(choice) {
+    if (choice === answer) {
         console.log('WIN');
+        alert("Nice! Keep going!");
         resetAnswer();
+        getQuestion();
+        addSeconds = timeRemaining + 5;
+        $timer.html(addSeconds);
     } else {
         console.log('LOSE');
+        alert("Try again whelp");
+        resetAnswer();
+        getQuestion();
     }
 };
 
+let removeWelcome = function(){
+  $(".play").hide();
+  getQuestion();
+};
 
 //Resets Question set
 let resetAnswer = function() {
@@ -70,47 +81,38 @@ let resetAnswer = function() {
 };
 
 
-// Elements clicks
+//countdown
+let countDown = setInterval(function() {
+    if (timeRemaining <=1){
+      clearInterval(countDown);
+    }
+    timeRemaining--;
+    $timer.html(timeRemaining);
+    console.log(timeRemaining);
+}, 1000);
+
+
+// Event handlers
 $ansA.on('click', function() {
     console.log('testing answer');
     let choice = $ansA.text();
-    correctAnswer();
+    correctAnswer(choice);
 });
-
-
 $ansB.on('click', function() {
     console.log('test');
     let choice = $ansB.text();
-    correctAnswer();
+    correctAnswer(choice);
 });
-
-
 $ansC.on('click', function() {
     console.log('test');
     let choice = $ansC.text();
-    correctAnswer();
+    correctAnswer(choice);
 });
-
-
 $ansD.on('click', function() {
     console.log('test');
     let choice = $ansD.text();
-    correctAnswer();
+    correctAnswer(choice);
 });
 
-$('#start').on('click', getQuestion);
+$('.play').on('click', removeWelcome);
 $('#restart').on('click', resetAnswer);
-
-
-
-
-//Questions array
-const questionBank = [
-    ['Who is the "Chosen One"?', ['Agent Smith', 'Neo', 'Harry Potter', 'The Rock']],
-    ["Who is Harry Potter's Defense Against The Dark Arts professor?", ['Severus Snape', 'Agent Smith', 'Neo', 'Harry Potter, himself']],
-    ['What is the name of the story Bilbo wrote about his adventures?', ['The Hobbit by Bilbo Baggins', 'The Silmarillion by Bilbo Baggins', 'A Hobbits Tale by Bilbo Baggins', 'Into the West by Bilbo Baggins']],
-    ['By what name do the Elves call Gandalf?', ['Mithrandir', 'Gandalf the Grey', 'Incanus', 'The Grey Pilgrim']],
-    ['What three swords were found in the Trolls Cave in The Hobbit?', ['Orcrist, Sting and Glamdring', 'Narsil, Glamdring and Hadhafang', 'Sting, Anduil and The White Knife of Legolas', 'Aeglos, Orcrist and Sting']],
-    ['Who is referred to as the Son of Glion?', ['Gimli', 'Durin', 'Balin', 'Legolas']],
-    ['What is "...as hard as dragon scale and as light as a feather"?', ['Mithril', 'Adamantium', 'Titanuim', 'The Rock']]
-];
